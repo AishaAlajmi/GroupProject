df86a9a8905f1ba43b62ccbd60855defacdb5139
Jamelah Hadi <108652057+Jamelah1420@users.noreply.github.com>
2023-09-02 18:52:46 +0300

    add mes

--- src/CPIT251.java
+++ src/CPIT251.java
@@ -14,7 +14,7 @@ public class CPIT251 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Aisha Almi - 2105522");
     }
-    
+
 }
