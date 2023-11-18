15644f4bfa9947a80610c241e362c02b9fe2b3ef
Jamelah Hadi <108652057+Jamelah1420@users.noreply.github.com>
2023-11-18 20:38:01 +0300

    NEW UPDATE

---
+++ src/GroupProject.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+class CPIT251 {
+
+
+    public static void main(String[] args) {
+        System.out.println("aAISHA");
+    }
+     public static void AddReport() {
+
+    }
+
+    public static void DisplayAllStudentReports() {
+
+    }
+
+    public static void DeleteReport() {
+
+    }
+}
